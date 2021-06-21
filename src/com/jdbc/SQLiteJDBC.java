f66766ed4f3940a88dd35df88191c4d7a94e6585
timofeya123 <timofeya123@inbox.ru>
2021-06-21 20:35:07 +0600

    comment

--- src/com/jdbc/SQLiteJDBC.java
+++ src/com/jdbc/SQLiteJDBC.java
@@ -4,13 +4,13 @@ import java.sql.*;
 
 public class SQLiteJDBC {
     public static void main( String args[] ) {
-        Connection c = null;
+        Connection c = null; // соединение
 
         try {
-            Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:test.db");
+            Class.forName("org.sqlite.JDBC"); // формат бд
+            c = DriverManager.getConnection("jdbc:sqlite:test.db"); // выбираем бд из фалов и соединяемся
         } catch ( Exception e ) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() ); // ошибка
             System.exit(0);
         }
         System.out.println("БД открыта, успех!");
